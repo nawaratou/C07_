694060efc8ab85d72a82b92c0be841fccbe48263
Ousseini <ousseini@Ousseinis-iMac.local>
2025-08-18 14:53:04 +0100

    premier commit

---
+++ replace_char.c
@@ -0,0 +1,26 @@
+// replace_char.c
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char line[300];
+    char str[200];
+    char ch1, ch2;
+
+    // Lire la ligne entière : chaîne + deux caractères
+    fgets(line, sizeof(line), stdin);
+
+    // Extraire la chaîne et les deux caractères
+    // On suppose format : chaîne (sans espace) + ' ' + char + ' ' + char
+    sscanf(line, "%s %c %c", str, &ch1, &ch2);
+
+    // Remplacer dans la chaîne
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ch1) {
+            str[i] = ch2;
+        }
+    }
+
+    printf("Résultat : %s\n", str);
+    return 0;
+}
